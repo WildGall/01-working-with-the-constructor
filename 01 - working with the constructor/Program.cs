c75979346617e93f0ed67d218a31268f93999ef6
pekcr <pekcr@DESKTOP-QK2CH93>
2023-02-14 13:21:47 +0300

    Добавьте файлы проекта.

---
+++ 01 - working with the constructor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___working_with_the_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = { new Player("PlayerOne", 100, 50, 10, 5), new Player("PlayerTwo", 70, 60, 15, 7) };
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i].ShowInformation();
+            }
+        }
+    }
+
+    class Player
+    {
+        private string _gamerName;
+        private int _gamerHealth;
+        private int _gamerStamina;
+        private int _gamerDamage;
+        private int _gamerArmore;
+
+        public Player(string gamerName, int gamerHealth, int gamerStamina, int gamerDamage, int gamerArmore)
+        {
+            _gamerName = gamerName;
+            _gamerHealth = gamerHealth;
+            _gamerStamina = gamerStamina;
+            _gamerDamage = gamerDamage;
+            _gamerArmore = gamerArmore;
+        } 
+        
+        public void ShowInformation()
+        {
+            Console.WriteLine($"Игрок:{_gamerName}, Здровье:{_gamerHealth}, Выносливость: {_gamerStamina}, Урон: {_gamerDamage}, Броня: {_gamerArmore}");
+        }
+    }
+}
---
+++ 01 - working with the constructor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("01 - working with the constructor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01 - working with the constructor")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e8447aef-6f40-45d5-a94c-801dc9b7c3b0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
